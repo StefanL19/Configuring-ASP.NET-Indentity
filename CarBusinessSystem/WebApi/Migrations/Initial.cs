bd0c709f76c1ffc38cfbb18047eaea467b76bb34
StefanL19 <stlazov@abv.bg>
2017-01-02 12:31:44 +0200

    Add sample migration

---
+++ CarBusinessSystem/WebApi/Migrations/Initial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Migrations
+{
+    public class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            //Put the code for migration here
+        }
+    }
+}
